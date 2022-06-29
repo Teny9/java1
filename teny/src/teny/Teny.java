d5d21e18300247f30817cc8868984edad8f84715
teny <salt535353@naver.com>
2022-06-29 21:30:45 +0900

    println with minsoo!

--- teny/src/teny/Teny.java
+++ teny/src/teny/Teny.java
@@ -5,6 +5,9 @@ public class Teny {
 	public static void main(String[] args) {
 		// 
 		System.out.println("\\"+" "+"/\\");
+		System.out.println("\\"+" "+"/\\");
+		System.out.println("\\"+" "+"/\\");
+		System.out.println("\\"+" "+"/\\");
 		
 	}
 
